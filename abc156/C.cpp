cb8be2f72d08b94d1ef51b5ab67455c60d562873
Ayaka <ayk.04ls.o0@gmail.com>
2020-04-28 15:08:43 +0900

    :: abc156 added

---
+++ abc156/C.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+
+#define mem0(x) memset(x, 0, sizeof(x))
+#define asort(x) sort(x.begin(), x.end());
+#define dsort(x, t) sort(x.begin(), a.end(), greater<t>());
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+#define MINS(m, s) m.insert(mp(s, 1));
+#define MFIN(m, s) m.find(s) != m.end()
+#define mp make_pair
+#define pb push_back
+
+int main() {
+    ll N, hp;
+    ll ans = 100000000000LL;
+    cin >> N;
+    int X[101];
+    for(ll i = 0; i < N; i++) {
+        cin >> X[i];
+    }
+    for(ll i = 1; i <= 100; i++) {
+        hp = 0;
+        for(ll j = 0; j < N; j++) {
+            hp += pow((X[j] - i), 2);
+        }
+        ans = min(ans, hp);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
--- abc164/D.cpp
+++ abc164/D.cpp
@@ -9,11 +9,18 @@ typedef pair<ll, ll> pll;
 typedef vector<pii> vii;
 typedef vector<pll> vll;
 
-#define mem0(x) memset(x, 0, sizeof(x))
-#define asort(x) sort(x.begin(), x.end());
-#define dsort(x, t) sort(x.begin(), a.end(), greater<t>());
 #define mp make_pair
 #define pb push_back
+#define F first
+#define S second
+
+#define all(x) (x).begin(), (x).end()
+#define m0(x) memset(x, 0, sizeof(x))
+#define asort(x) sort(x.begin(), x.end());
+#define dsort(x, t) sort(x.begin(), a.end(), greater<t>());
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+
+const int mod = 1e9 + 7;
 
 int main() {
     int cnt = 0;
@@ -21,7 +28,7 @@ int main() {
     cin >> S;
     for(int i = 0; i < S.length(); i++) {
         for(int j = 1; j <= S.length() - i; j++) {
-            if(atoi(S.substr(i, j).c_str()) % 2019 == 0) {
+            if(strtol(S.substr(i, j).c_str(), NULL, 10) % 2019 == 0) {
                 cnt++;
                 break;
             }
