254d58111a82445029f51f3285710535022a03ce
Ayaka <ayk.04ls.o0@gmail.com>
2020-04-29 02:08:07 +0900

    :: abc158 added

---
+++ abc158/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+
+#define mem0(x) memset(x, 0, sizeof(x))
+#define asort(x) sort(x.begin(), x.end());
+#define dsort(x, t) sort(x.begin(), a.end(), greater<t>());
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+#define MINS(m, s) m.insert(mp(s, 1));
+#define MFIN(m, s) m.find(s) != m.end()
+#define mp make_pair
+#define pb push_back
+#define INF (1 << 29)
+const int mod = 1000000007;
+
+int main() {
+    int flag;
+    string S;
+    cin >> S;
+    if(S[0] == S[1]){
+        if(S[1] == S[2]){
+        cout << "No" <<  endl;
+        }else{
+            cout << "Yes" << endl;
+        }
+    }else{
+        cout << "Yes" << endl;
+    }
+
+    return 0; 
+}
