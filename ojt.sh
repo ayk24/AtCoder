5be4d140a6aec1b54898e83df9c67da7d1168d64
ayk24 <ayk.04ls.o0@gmail.com>
2021-01-06 03:44:20 +0900

    :: fix ojt.sh

--- ojt.sh
+++ ojt.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 contest_name=$1
 problem_name=$2
 test_dir=test/${problem_name}
 
-# make test directory
 if [ ! -e ${test_dir} ]; then
     oj dl -d test/${problem_name} https://atcoder.jp/contests/${contest_name}/tasks/${contest_name//-/_}_${problem_name}
 fi
 
-g++ ${problem_name^^}.cpp
+g++ ${problem_name}.cpp
 oj test -c "./a.out" -d test/${problem_name}
